e100e133879161a0fbd5a9cd40c7ed480c1fd436
Eiji MATSUMOTO <e.mattsan@gmail.com>
2019-11-29 21:45:50 +0900

    initial commit

---
+++ lib/ex_irb.ex
@@ -0,0 +1,18 @@
+defmodule ExIRB do
+  @moduledoc """
+  Documentation for ExIRB.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExIRB.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_irb_test.exs
@@ -0,0 +1,8 @@
+defmodule ExIRBTest do
+  use ExUnit.Case
+  doctest ExIRB
+
+  test "greets the world" do
+    assert ExIRB.hello() == :world
+  end
+end
